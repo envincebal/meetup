4656eb5caff72955d0efa9bbf528d5c61271ef68
envincebal <vincentyan1@gmail.com>
2020-01-17 01:33:23 -0800

    Added Atatus Coninuous Delivery Testing.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './App';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import * as atatus from 'atatus-spa';
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+atatus.config('82e9337550b64180b81f6a33d1c8608e').install();
 serviceWorker.unregister();
+
+// atatus.notify(new Error('Test Atatus Setup')); 
