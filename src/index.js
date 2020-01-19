8d91403996b36b7b0b49ca70d042e4c9ac7181e6
envincebal <vincentyan1@gmail.com>
2020-01-18 18:42:10 -0800

    Added service worker.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as atatus from 'atatus-spa';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 atatus.config('82e9337550b64180b81f6a33d1c8608e').install();
-serviceWorker.unregister();
+serviceWorker.register();
+// serviceWorker.unregister();
 
 // atatus.notify(new Error('Test Atatus Setup')); 
